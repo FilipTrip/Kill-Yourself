d045c9715ece463b2cef631b05ebf3708a5a33bc
FilipTrip <81421618+FilipTrip@users.noreply.github.com>
2023-04-01 18:20:49 +0200

    Game loop

--- Kill Yourself/Assets/Scripts/Managers/GameManager.cs
+++ Kill Yourself/Assets/Scripts/Managers/GameManager.cs
@@ -2,16 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance { get; private set; }
 
     [SerializeField] private float gravity = -1;
+    [SerializeField] private TextMeshProUGUI levelText;
 
     private void Awake()
     {
+        Instance = this;
         Physics2D.gravity = new Vector2(0f, gravity);
+        levelText.text = SceneManager.GetActiveScene().name;
     }
 
     public void LevelCompleted()
